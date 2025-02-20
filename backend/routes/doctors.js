8098c4132a9d3d217437648d4a1e55b7dbf5c859
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-02-21 01:17:49 +0530

    Route Structure

---
+++ backend/routes/doctors.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const doctorController = require('../controllers/doctorController');
+
+router.get('/', doctorController.getAllDoctors);
+router.get('/:id', doctorController.getDoctor);
+router.post('/', doctorController.createDoctor);
+router.get('/:id/slots', doctorController.getAvailableSlots);
+
+module.exports = router;
